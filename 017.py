3a387c05b2cf6d848d4f5e612185819ba52daba0
timsergor <50012516+timsergor@users.noreply.github.com>
2019-06-21 14:34:56 +0300

    Update 017.py

--- 017.py
+++ 017.py
@@ -1,3 +1,6 @@
+#Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+#If there are two middle nodes, return the second middle node.
+ 
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
