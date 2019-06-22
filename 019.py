4cc5f46bc929fc9e74971d014d2e3c486dbd9cd3
timsergor <50012516+timsergor@users.noreply.github.com>
2019-06-22 13:27:56 +0300

    Create 019.py

---
+++ 019.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shortestToChar(self, S: str, C: str):
+        M = [10000]*len(S)
+        last = -10000
+        i = 0
+        while i < len(S):
+            if S[i] != C:
+                M[i] = i - last
+                i += 1    
+            else:
+                last = i
+                while i > -1:
+                    if M[i] > (last-i):
+                        M[i] = last-i
+                        i -= 1
+                    else:
+                        i = last + 1
+                        break 
+                if i == -1:        
+                    i = last + 1        
+        if last > -1:
+            for i in range(last,len(S)):
+                M[i] = i - last
+        return(M) 
