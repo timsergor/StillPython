cb888bbe444377043e0cb10c06bf202fa2eecdac
timsergor <50012516+timsergor@users.noreply.github.com>
2019-06-26 17:20:08 +0300

    Create 022.py

---
+++ 022.py
@@ -0,0 +1,15 @@
+#Alice and Bob take turns playing a game, with Alice starting first.
+
+#Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
+
+#Choosing any x with 0 < x < N and N % x == 0.
+#Replacing the number N on the chalkboard with N - x.
+#Also, if a player cannot make a move, they lose the game.
+
+#Return True if and only if Alice wins the game, assuming both players play optimally.
+
+class Solution:
+    def divisorGame(self, N: int):
+        if N % 2:
+            return(False)
+        return(True)
