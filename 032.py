59ce31fc8da9970421ceaa388b9726179b498747
timsergor <50012516+timsergor@users.noreply.github.com>
2019-07-23 19:43:41 +0300

    Update 032.py

--- 032.py
+++ 032.py
@@ -1,3 +1,9 @@
+# 1022. Sum of Root To Leaf Binary Numbers. Easy, 57.4%
+
+# Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+#For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+#Return the sum of these numbers.
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -53,3 +59,5 @@ class Solution(object):
             return(M,S)
         M,S = somewhat(root)
         return(S)
+
+# 30-140 min
