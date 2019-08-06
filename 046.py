e445d8edd22831442c60114fbcfc86b539dc2d86
timsergor <50012516+timsergor@users.noreply.github.com>
2019-08-07 01:46:43 +0300

    Update 046.py

--- 046.py
+++ 046.py
@@ -1,3 +1,6 @@
+# 541. Reverse String II. Easy. 45.8%
+# Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original
+
 class Solution:
     def reverseStr(self, s: str, k: int):
         i = 0
