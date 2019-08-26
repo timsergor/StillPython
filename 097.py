e21c38e1f0cfea0c2c5cd168e8fa84a398cd46a2
timsergor <50012516+timsergor@users.noreply.github.com>
2019-08-26 13:20:30 +0300

    Update 097.py

--- 097.py
+++ 097.py
@@ -1,3 +1,8 @@
+#969. Pancake Sorting. Medium. 62.1%.
+
+#Given an array A, we can perform a pancake flip: We choose some positive integer k <= A.length, then reverse the order of the first k elements of A.  We want to perform zero or more pancake flips (doing them one after another in succession) to sort the array A.
+#Return the k-values corresponding to a sequence of pancake flips that sort A.  Any valid answer that sorts the array within 10 * A.length flips will be judged as correct.
+
 class Solution:
     def pancakeSort(self, A: List[int]) -> List[int]:
         def rev(M,n):
