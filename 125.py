1e3dee4703f0006a713d9da05c6639e5a5b920f5
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-02 12:18:13 +0300

    Update 125.py

--- 125.py
+++ 125.py
@@ -1,4 +1,4 @@
-# 20. Valid Parentheses. Easy.
+# 20. Valid Parentheses. Easy. 37.`%.
 
 #Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 #An input string is valid if:
