d4138ab8cce17d03e62587f87ee0341e0a6b63aa
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-04 11:17:42 +0300

    Update 132.py

--- 132.py
+++ 132.py
@@ -1,4 +1,4 @@
-# 970. Powerful Integers. Easy.
+# 970. Powerful Integers. Easy. 39.6%.
 # Given two positive integers x and y, an integer is powerful if it is equal to x^i + y^j for some integers i >= 0 and j >= 0.
 # Return a list of all powerful integers that have value less than or equal to bound.
 # You may return the answer in any order.  In your answer, each value should occur at most once.
