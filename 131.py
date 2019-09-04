15e859f1b0d07f371e2c59eb4acb1135dc290d72
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-04 10:35:59 +0300

    Create 131.py

---
+++ 131.py
@@ -0,0 +1,33 @@
+class Solution(object):        
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        def xor(x,y,z):
+            if (x * y * z == 1) or (x + y + z == 1):
+                return 1
+            else:
+                return 0
+        
+        pre = []
+        mem = 0
+        for i in range(max(len(a),len(b))):
+            if len(a) -1 - i < 0:
+                A = 0
+            else:
+                A = int(a[len(a) -1 - i])
+            if len(b) -1 - i < 0:
+                B = 0
+            else:
+                B = int(b[len(b) -1 - i])
+            pre.append(str(xor(A,B,mem)))
+            if A + B + mem > 1:
+                mem = 1
+            else:
+                mem = 0
+        if mem == 1:
+            pre.append(str(mem))
+        pre.reverse()
+        return("".join(pre))
