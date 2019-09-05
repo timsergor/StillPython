e36b07ed304f6934df45b745934d1483bf9c84e3
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-05 12:38:58 +0300

    Update 140.py

--- 140.py
+++ 140.py
@@ -1,3 +1,12 @@
+# 1035. Uncrossed Lines. Medium. 51.9%.
+
+# We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+# Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
+# A[i] == B[j];
+# The line we draw does not intersect any other connecting (non-horizontal) line.
+# Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
+# Return the maximum number of connecting lines we can draw in this way.
+
 class Solution(object):
     def maxUncrossedLines(self, A, B):
         """
