be94fa10480a52d85857c6c6cf02ddc06e5e7cc4
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-11 23:24:49 +0300

    Create 153.py

---
+++ 153.py
@@ -0,0 +1,36 @@
+# 872. Leaf-Similar Trees. Easy.
+
+#Consider all the leaves of a binary tree.  From left to right order, the values of those leaves form a leaf value sequence.
+# For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+# Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+# Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        def lvs(node):
+            if node == None:
+                return []
+            elif not (node.left or node.right):
+                return [node.val]
+            else:
+                L = lvs(node.left)
+                R = lvs(node.right)
+                L.extend(R)
+                return L
+        
+        A = lvs(root1)
+        B = lvs(root2)
+        return A == B
+        
+# <3min.
