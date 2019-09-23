cfa1d22d2971de5c7ae7019a287d1d9bcf4fd770
timsergor <50012516+timsergor@users.noreply.github.com>
2019-09-23 11:25:39 +0300

    Create 185.py

---
+++ 185.py
@@ -0,0 +1,30 @@
+# 73. Set Matrix Zeroes
+
+# Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][j] = "*"
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == "*":
+                    for k in range(len(matrix)):
+                        if matrix[k][j] != "*":
+                            matrix[k][j] = 0
+                    for k in range(len(matrix[0])):
+                        if matrix[i][k] != "*":
+                            matrix[i][k] = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == "*":
+                    matrix[i][j] = 0
+        return matrix
+        
+# 5min.
