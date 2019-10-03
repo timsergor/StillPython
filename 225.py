981a818ff3a50b9c3329a846c5d322b30c4bad50
timsergor <50012516+timsergor@users.noreply.github.com>
2019-10-03 20:43:22 +0300

    Update 225.py

--- 225.py
+++ 225.py
@@ -1,4 +1,23 @@
-if len(nums) == 0:
+# 33. Search in Rotated Sorted Array. Medium. 33.2%.
+
+# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+# (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+# You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+# You may assume no duplicate exists in the array.
+
+# Your algorithm's runtime complexity must be in the order of O(log n).
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
             return -1
         
         def search(l,r):
