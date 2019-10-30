7408c1bde3a44076b45b6fef7a8f59f0e5f84f5c
timsergor <50012516+timsergor@users.noreply.github.com>
2019-10-30 17:48:26 +0300

    Create 333.py

---
+++ 333.py
@@ -0,0 +1,26 @@
+# 98. Validate Binary Search Tree
+
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+
+# Assume a BST is defined as follows:
+
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def isIt(node, down = "#", up = "#"):
+            if not node:
+                return True
+            Flag = (down == "#" or down < node.val) and (up == "#" or up > node.val)
+            return Flag and isIt(node.left, down, node.val) and isIt(node.right, node.val, up)
+        
+        return isIt(root)
