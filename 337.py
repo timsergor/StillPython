a1dcb70cca08037e061dce2b0547cc8b6c53bf41
timsergor <50012516+timsergor@users.noreply.github.com>
2019-10-30 20:22:09 +0300

    Update 337.py

--- 337.py
+++ 337.py
@@ -1,6 +1,6 @@
 # 357. Count Numbers with Unique Digits. 47.6%.
 
-# Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+# Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10**n.
 
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
