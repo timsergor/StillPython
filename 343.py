7a45ec176f0cb7895dd95eda21e141e97c3a0410
timsergor <50012516+timsergor@users.noreply.github.com>
2019-11-01 00:33:59 +0300

    Create 343.py

---
+++ 343.py
@@ -0,0 +1,45 @@
+# 155. Min Stack. Easy. 39.4%.
+
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if len(self.stack) == 1 or x < self.minStack[-1]:
+            self.minStack.append(x)
+        else:
+            self.minStack.append(self.minStack[-1])
+                
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
