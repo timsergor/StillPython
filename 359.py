1b95b0cd09553bb72543db42aef0ecf435c3b1ee
timsergor <50012516+timsergor@users.noreply.github.com>
2019-11-04 12:50:11 +0300

    Create 359.py

---
+++ 359.py
@@ -0,0 +1,28 @@
+# 299. Bulls and Cows. Easy. 40.8%.
+
+# You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+# Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
+
+# Please note that both secret number and friend's guess may contain duplicate digits.
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        Bulls = []
+        ost = {}
+        for i in range(len(secret)):
+            if secret[i] == guess[i]:
+                Bulls.append(i)
+            else:
+                if secret[i] not in ost:
+                    ost[secret[i]] = 1
+                else:
+                    ost[secret[i]] += 1
+        for i in range(len(secret)):
+            if i not in Bulls and guess[i] in ost:
+                if ost[guess[i]]:
+                    ost[guess[i]] -= 1
+        B = 0
+        for c in ost:
+            B += ost[c]
+        return str(len(Bulls)) + "A" + str(len(secret) - len(Bulls) - B) + "B"
