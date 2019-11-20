da24a9f168508af63898df536996a1e467e8a2b3
timsergor <50012516+timsergor@users.noreply.github.com>
2019-11-20 20:52:29 +0300

    Create 429.py

---
+++ 429.py
@@ -0,0 +1,35 @@
+# 884. Uncommon Words from Two Sentences. Easy.
+
+# We are given two sentences A and B.  (A sentence is a string of space separated words.  Each word consists only of lowercase letters.)
+
+# A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+# Return a list of all uncommon words. 
+
+# You may return the list in any order.
+
+class Solution(object):
+    def uncommonFromSentences(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: List[str]
+        """
+        A = A.split()
+        B = B.split()
+        C = {}
+        for w in A:
+            if w in C:
+                C[w] += 1
+            else:
+                C[w] = 1
+        for w in B:
+            if w in C:
+                C[w] += 1
+            else:
+                C[w] = 1
+        answer = []
+        for w in C:
+            if C[w] == 1:
+                answer.append(w)
+        return answer
